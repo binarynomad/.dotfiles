9c5e26d37e5b10819e7d8d535e0b5157ec713f5b
binarynomad <binarynomad@gmail.com>
2024-07-24 15:49:46 -0700

    corrected type in mac-init and added status messages, updated README.md with prerequisites

--- .config/yadm/mac-init.sh
+++ .config/yadm/mac-init.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 # Start the Brew Installer which will also install Xcode/git
+echo "Installing Xcode, Git, & Brew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Temporarily envoke the brew environment, this will be replaced
 # by the YADM entry in .zprofile
-eval "$(/opt/homebrew/bin/brew shellenve)"
+echo "Envoking BREW enviroment..."
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Use Brew to install YADM
+echo "Installing YADM..."
 brew install yadm
 
 # Use YADM to pull the rest of the ENV and Bootstrap files
+echo "Activating YADM, calling bootstrap scripts..."
 yadm clone https://github.com/binarynomad/.dotfiles.git
 
