1698b37c01dd9afa36cad7d51f609f1264ece30b
binarynomad <binarynomad@gmail.com>
2024-07-25 21:29:35 -0700

    added LANGUAGE line and more debug cleanup

--- .config/yadm/bootstrap.d/bootstrap-linux.sh
+++ .config/yadm/bootstrap.d/bootstrap-linux.sh
@@ -8,6 +8,7 @@ export OS_TYPE="$(uname -s)"
 if [[ "$OS_TYPE" != "Linux" ]]; then echo "Skipping Linux scripts..."; exit 0; fi
 
 # Beginning Installation
+clear
 echo "-------------------------------------------------------"
 echo "Starting Bootstrap install for (${OS_TYPE})"
 echo "-------------------------------------------------------"
@@ -34,20 +35,18 @@ done
 
 # Prepare OS
 echo ""
-echo "Updating APT system..."; sudo apt update
-echo "Upgrading APT system..."; sudo apt upgrade -y
-echo "Autoremove APT system..."; sudo apt autoremove -y
-echo "Autoclean APT system..."; sudo apt autoclean -y
+echo -e "\nUpdating APT system..."; sudo apt update
+echo -e "\nUpgrading APT system..."; sudo apt upgrade -y
+echo -e "\nAutoremove APT system..."; sudo apt autoremove -y
+echo -e "\nAutoclean APT system..."; sudo apt autoclean -y
 
 
 # Install some common CLI tools
 echo ""
 echo "Installing common CLI tools..."
 packages="bat fd-find jq ripgrep tldr"
-apt update
 for package in $packages; do
-    echo ""
-    echo "Installing ${package}"
+    echo -e "\nInstalling ${package}"
     sudo apt install -y $package
 done
 
@@ -55,9 +54,10 @@ done
 # APPS CONFIG:
 # ---------------------------------------------
 
-echo "Linking some common CLI tools..."
+echo -e "\nLinking some common CLI tools..."
+
 # FD: Need to setup a link to fdfind so exports work properly.
-echo "Linking FD..." ; ln -s $(which fdfind) ${HOME}/bin/fd
+echo -e "\nLinking FD..." ; ln -s $(which fdfind) ${HOME}/bin/fd
 
 # BAT: Need to setup a link to batcat so exports work properly.
-echo "Linking BAT..." ; ln -s $(which batcat) ${HOME}/bin/bat
+echo -e "\nLinking BAT..." ; ln -s $(which batcat) ${HOME}/bin/bat
