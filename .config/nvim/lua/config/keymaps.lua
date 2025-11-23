eed1935267ca60d3fb1034914ee157f84f7b9364
binarynomad <binarynomad@gmail.com>
2025-11-22 22:03:10 -0800

    updated lazyvim in a do-over

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "kj", "<ESC>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap("n", "<SPACE>;", ":", { noremap = true, silent = true })
+-- Setup alternative ESC keys
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
+
+-- Setup alternative COMMND key
+vim.keymap.set("n", "<SPACE>;", ":", { noremap = true, silent = true })
+
+-- Setup alternative QUIT key
+vim.keymap.set("c", "qq", "q!", { noremap = true, silent = true })
+
+-- Disable default 'q' to prevent accidental macro recording; remap 'Q' to toggle instead.
+vim.keymap.set("n", "q", "<nop>", { noremap = true, silent = true }) -- Disable original 'q' mapping
+vim.keymap.set("n", "Q", "q", { noremap = true, desc = "Start/Stop Macro Recording" }) -- Map 'Q' to 'q'
