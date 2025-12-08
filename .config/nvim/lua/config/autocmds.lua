e4a0c4252d8561bff4c234e630a825cf5783f92f
binarynomad <binarynomad@gmail.com>
2025-12-07 21:16:19 -0800

    keymaps for diffsaved and some descriptions

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Command to see unsaved changes side by side
+vim.api.nvim_create_user_command("DiffSaved2", function()
+  local filetype = vim.bo.filetype
+  vim.cmd("diffthis")
+  vim.cmd("vnew | r # | normal! 1Gdd")
+  vim.cmd("diffthis")
+  vim.cmd("setlocal bt=nofile bh=wipe nobl noswf ro ft=" .. filetype)
+end, {})
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,11 +8,15 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 vim.keymap.set("i", "kj", "<Esc>", { noremap = true, silent = true })
 
 -- Setup alternative COMMND key
-vim.keymap.set("n", "<SPACE>;", ":", { noremap = true, silent = true })
+vim.keymap.set("n", "<SPACE>;", ":", { noremap = true, silent = true, desc = "Alternative cmdline" })
 
 -- Setup alternative QUIT key
-vim.keymap.set("c", "qq", "q!", { noremap = true, silent = true })
+vim.keymap.set("c", "qq", "q!", { noremap = true, silent = true, desc = "Alternative forced quit" })
 
 -- Disable default 'q' to prevent accidental macro recording; remap 'Q' to toggle instead.
 vim.keymap.set("n", "q", "<nop>", { noremap = true, silent = true }) -- Disable original 'q' mapping
 vim.keymap.set("n", "Q", "q", { noremap = true, desc = "Start/Stop Macro Recording" }) -- Map 'Q' to 'q'
+
+-- Setup a next buffer sub-command
+-- vim.keymap.set("n", "<LEADER>bn", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+vim.keymap.set("n", "<LEADER>bn", ":enew<CR>", { noremap = true, desc = "New Buffer" })
